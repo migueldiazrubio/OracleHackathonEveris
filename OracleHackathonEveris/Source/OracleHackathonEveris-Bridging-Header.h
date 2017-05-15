d49653f7c82605a56b489507f7dad1c2bf3b5733
Paul Alava Doncel <paul.alava.doncel@everis.com>
2017-05-15 20:52:26 +0200

    bridge

---
+++ OracleHackathonEveris/Source/OracleHackathonEveris-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  OracleHackathonEveris-Bridging-Header.h
+//  OracleHackathonEveris
+//
+//  Created by Paul Alava Doncel on 15/5/17.
+//  Copyright © 2017 everis. All rights reserved.
+//
+
+#ifndef OracleHackathonEveris_Bridging_Header_h
+#define OracleHackathonEveris_Bridging_Header_h
+
+
+#endif /* OracleHackathonEveris_Bridging_Header_h */
