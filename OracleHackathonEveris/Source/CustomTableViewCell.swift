a7da421b2de096cefaa17dbb3e60e2164732412c
Paul Alava Doncel <paul.alava.doncel@everis.com>
2017-05-16 14:36:49 +0200

    Added Table

---
+++ OracleHackathonEveris/Source/CustomTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTableViewCell.swift
+//  OracleHackathonEveris
+//
+//  Created by Paul Alava Doncel on 16/5/17.
+//  Copyright © 2017 everis. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelAddress: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
